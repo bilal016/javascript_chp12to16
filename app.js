1855017c057871a7a4b5cdb0b6e0d14d3a461593
Bilal Shah <syedbilalshah016@gmail.com>
2024-03-16 03:22:38 +0500

    Chapter 12 done

--- app.js
+++ app.js
@@ -82,19 +82,20 @@
 
 
 // // #7
-var time = +prompt("Enter Time", "2000");
-if(time >= 0000 && time < 1200){
-    document.write("Good Morning!")
-}
-else if(time >= 1200 && time < 1700){
-    document.write("Good Afternoon!")
-}
-else if(time >= 1700 && time < 2100){
-    document.write("Good Evening!")
-}
-else if(time >= 2100 && time <= 2359){
-document.write("Good Night!")
-}
-else{
-    document.write("Inalid Time")
-}
+// var time = +prompt("Enter Time", "2000");
+// if(time >= 0000 && time < 1200){
+//     document.write("Good Morning!")
+// }
+// else if(time >= 1200 && time < 1700){
+//     document.write("Good Afternoon!")
+// }
+// else if(time >= 1700 && time < 2100){
+//     document.write("Good Evening!")
+// }
+// else if(time >= 2100 && time <= 2359){
+// document.write("Good Night!")
+// }
+// else{
+//     document.write("Invalid Time")
+// }
+
